e9d9c2d2d149187196b52d6fd4c4e9c3d05f035a
LucasLdp <lucasdepauloldp1@gmail.com>
2026-03-24 16:05:10 -0300

    feat: update GroupsScreen to display header and goals list

--- lib/screens/groups/page/groups_screen.dart
+++ lib/screens/groups/page/groups_screen.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:splitty/screens/groups/sections/groups_header.dart';
+import 'package:splitty/screens/groups/sections/groups_saving_goals_list.dart';
 
 class GroupsScreen extends StatelessWidget {
   const GroupsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Groups'));
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: const [
+        GroupsHeader(),
+        Expanded(child: GroupsSavingGoalsList()),
+      ],
+    );
   }
 }
