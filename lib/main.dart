f92abe3e65d155beef3b7fd0fbbe89414edb01a4
LucasLdp <lucasdepauloldp1@gmail.com>
2026-03-24 11:53:03 -0300

    refactor: replace MyApp with SplittyApp in main entry point

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:splitty/screens/auth/page/auth_screen.dart';
-import 'package:splitty/utils/colors.dart';
+import 'package:splitty/app.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: AppColors.primary,
-        scaffoldBackgroundColor: AppColors.background,
-      ),
-      home: const AuthScreen(),
-    );
-  }
+  runApp(const SplittyApp());
 }
