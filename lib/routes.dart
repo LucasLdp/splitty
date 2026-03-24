883ba60772a06cb83a3323aa1e7d86f5567932d2
LucasLdp <lucasdepauloldp1@gmail.com>
2026-03-24 17:24:55 -0300

    chore: update routes to remove GroupsScreen and add GoalsScreen

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,7 +1,7 @@
 import 'package:go_router/go_router.dart';
 import 'package:splitty/screens/auth/page/auth_screen.dart';
 import 'package:splitty/screens/common/layouts/app_layout.dart';
-import 'package:splitty/screens/groups/page/groups_screen.dart';
+import 'package:splitty/screens/goals/page/goals_screen.dart';
 import 'package:splitty/screens/home/page/home_screen.dart';
 import 'package:splitty/screens/profile/page/profile_screen.dart';
 
@@ -13,10 +13,7 @@ class RouteConfig {
         builder: (ctx, state, child) => AppLayout(child: child),
         routes: [
           GoRoute(path: '/home', builder: (ctx, state) => const HomeScreen()),
-          GoRoute(
-            path: '/groups',
-            builder: (ctx, state) => const GroupsScreen(),
-          ),
+          GoRoute(path: '/goals', builder: (ctx, state) => const GoalsScreen()),
           GoRoute(
             path: '/profile',
             builder: (ctx, state) => const ProfileScreen(),
