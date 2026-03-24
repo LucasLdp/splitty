acdaec0412596130aec44471cfed8c666b51167f
LucasLdp <lucasdepauloldp1@gmail.com>
2026-03-24 16:05:27 -0300

    feat: update ProfileScreen to display profile sections

--- lib/screens/profile/page/profile_screen.dart
+++ lib/screens/profile/page/profile_screen.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:splitty/screens/profile/sections/profile_header.dart';
+import 'package:splitty/screens/profile/sections/profile_info.dart';
+import 'package:splitty/screens/profile/sections/profile_notification.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Perfil'));
+    return Column(
+      spacing: 32,
+      children: [ProfileHeader(), ProfileInfo(), ProfileNotification()],
+    );
   }
 }
