f59a7c1aba93c183787601b0f42b33a3070a1577
LucasLdp <lucasdepauloldp1@gmail.com>
2026-03-25 00:58:15 -0300

    feat: enhance CustomTitle widget with customizable properties

--- lib/screens/common/widgets/custom_title.dart
+++ lib/screens/common/widgets/custom_title.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CustomTitle extends StatelessWidget {
   final String title;
+  final double fontSize;
+  final int? maxLines;
+  final TextOverflow? overflow;
 
-  const CustomTitle({super.key, required this.title});
+  const CustomTitle({
+    super.key,
+    required this.title,
+    this.fontSize = 32,
+    this.maxLines,
+    this.overflow,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Text(
       title,
-      style: const TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+      maxLines: maxLines,
+      overflow: overflow,
+      style: GoogleFonts.sora(fontSize: fontSize, fontWeight: FontWeight.w800),
     );
   }
 }
